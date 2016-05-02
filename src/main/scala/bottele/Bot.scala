60352da2f2d7f9517803e89a819fcfb20a09084a
Alexey Romanchuk <al.romanchuk@2Gis.ru>
2016-05-02 19:39:09 +0600

    Extract scenarios

--- src/main/scala/bottele/Bot.scala
+++ src/main/scala/bottele/Bot.scala
@@ -6,6 +6,7 @@ import akka.stream._
 import akka.stream.scaladsl.{Keep, Sink}
 import bottele.TelegramBotAPI._
 import bottele.WebAPI.ApiError
+import bottele.scenarios.Router
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -15,8 +16,8 @@ object Bot extends App {
   implicit val ec = as.dispatcher
   implicit val m = ActorMaterializer(ActorMaterializerSettings(as))
 
-  val webApi = WebAPI(as)
-  val teleApi = TelegramBotAPI(as.settings.config.getString("telegram.token"))
+  implicit val webApi = WebAPI(as)
+  implicit val teleApi = TelegramBotAPI(as.settings.config.getString("telegram.token"))
 
   val (control, result) = UpdatesSource(teleApi)
     .map { x => println(s"Got update $x"); x }
@@ -27,7 +28,7 @@ object Bot extends App {
         teleApi.sendChatActionTyping(chatId).map(_ => msg)
       }.getOrElse(Future.successful(msg))
     }
-    .mapAsync(10)(Handler.apply)
+    .mapAsync(10) { update => Router.apply(update))
     .toMat(Sink.ignore)(Keep.both)
     .run()
 
@@ -38,107 +39,6 @@ object Bot extends App {
     case Failure(ex) =>
       ex.printStackTrace()
       sys.exit(1)
-  }
-  object Handler {
-    def apply(update: Update)(implicit ec: ExecutionContext): Future[Done] = update match {
-      case u if u.message.isDefined && u.message.get.text.isDefined =>
-        search(u.message.get.chat.id, u.message.get.text.get)
-      case Update(_ ,_ ,_, Some(callback)) =>
-        info(callback.from.id, callback.data, callback.id.toLong)
-      case _ => Future.successful(Done)
-    }
-
-    def info(chatId: Long, filial: String, callbackId: Long): Future[Done] = {
-      trait Reply
-      case class Location(lon: Double, lat: Double)
-      case class Info(name: String, phones: Iterable[String], location: Option[Location]) extends Reply
-      object NotFound extends Reply
-      object InternalError extends Reply
-      webApi
-        .branches(Seq(filial.toLong))
-        .map { branches =>
-          branches match {
-            case Left(ApiError(404, _, _)) => NotFound
-            case Left(ApiError(_, _, _)) => InternalError
-            case Right(branches) =>
-              branches.items.headOption.map { branch =>
-                val phones = for {
-                  groups <- branch.contact_groups.toSeq.flatten
-                  contact <- groups.contacts
-                  if contact.`type` == "phone"
-                } yield contact.text
-                val location = branch.point.map(p => Location(p.lon, p.lat))
-                Info(branch.name, phones, location)
-              }.getOrElse(NotFound)
-          }
-        }
-        .map {
-          case NotFound => teleApi.sendMessage(SendMessage(chatId, "Увы, мы ничего не нашли"))
-          case InternalError => teleApi.sendMessage(SendMessage(chatId, "Что-то пошло не так"))
-          case Info(name, phones, location) =>
-            val phonesText = Option(phones)
-              .filter(_.nonEmpty)
-              .map(_.mkString(", "))
-              .map(p => s"Позвонить: $p\n")
-              .getOrElse("")
-            val text = s"$name\n$phonesText"
-
-            for {
-              callbackF <- teleApi.answerCallbackQuery(callbackId)
-              msgF <- teleApi.sendMessage(SendMessage(chatId, text))
-              locatF <- location.map {
-                case Location(lon, lat) =>
-                  teleApi.sendLocation(SendLocation(chatId, lat, lon))
-              }.getOrElse(Future.successful(Done))
-            } yield Done
-        }
-        .map(_ => Done)
-    }
-
-    def search(chatId: Long, text: String): Future[Done] = {
-      case class Reply(chatId: Long, response: Response)
-      trait Response
-      object UnknownUpdate extends Response
-      object InternalError extends Response
-      object NotFound extends Response
-      case class Firm(id: String, name: String)
-      case class Firms(firms: List[Firm]) extends Response
-
-      def renderReply(reply: Reply) = reply.response match {
-        case InternalError => SendMessage(reply.chatId, "Что-то пошло не так")
-        case UnknownUpdate => SendMessage(reply.chatId, "Какое-то непонятное сообщение")
-        case NotFound => SendMessage(reply.chatId, "Увы, мы ничего не нашли")
-        case Firms(firms) =>
-          val markup = InlineKeyboardMarkup(
-            List(firms.zipWithIndex.map { case (f, i) => InlineKeyboardButton(s"${i + 1}", None, Some(f.id)) })
-          )
-          val text = firms.zipWithIndex.map {
-            case (f, i) => s"${i + 1}. ${f.name}"
-          }.mkString("\n")
-          SendMessage(reply.chatId, text, Some(markup))
-      }
-
-      webApi
-        .searchBranches(text, 1, page = Some(1), pageSize = Some(5))
-        .map { search =>
-          if (search.items.isEmpty) {
-            NotFound
-          }
-          else {
-            Firms(
-              search.items.toList.map { branch =>
-                val address = branch.address_name.map(address => s" ($address)").getOrElse("")
-                val id = branch.id.split('_').head
-                Firm(id, s"${branch.name}$address")
-              }
-            )
-          }
-        }
-        .fallbackTo(Future.successful(InternalError))
-        .map(reply => renderReply(Reply(chatId, reply)))
-        .flatMap(teleApi.sendMessage)
-        .map(_ => Done)
-    }
   }
 }
 
---
+++ src/main/scala/bottele/scenarios/Scenarios.scala
@@ -0,0 +1,111 @@
+package bottele.scenarios
+
+import akka.Done
+import bottele.{TelegramBotAPI, WebAPI}
+import bottele.TelegramBotAPI._
+import bottele.WebAPI.ApiError
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object Router {
+  def apply(update: Update)(implicit ec: ExecutionContext, teleApi: TelegramBotAPI, webApi: WebAPI): Future[Done] = update match {
+    case u if u.message.isDefined && u.message.get.text.isDefined =>
+      Search(u.message.get.chat.id, u.message.get.text.get)
+    case Update(_ ,_ ,_, Some(callback)) =>
+      Info(callback.from.id, callback.data, Some(callback.id.toLong))
+    case _ => Future.successful(Done)
+  }
+}
+
+object Info {
+  def apply(chatId: Long, filial: String, callbackId: Option[Long])(implicit ec: ExecutionContext, teleApi: TelegramBotAPI, webApi: WebAPI): Future[Done] = {
+    trait Reply
+    case class Location(lon: Double, lat: Double)
+    case class Info(name: String, phones: Iterable[String], location: Option[Location]) extends Reply
+    object NotFound extends Reply
+    webApi
+      .branches(Seq(filial.toLong))
+      .map { branches =>
+        branches match {
+          case Left(ApiError(404, _, _)) => NotFound
+          case Left(ApiError(_, _, _)) => InternalError
+          case Right(branches) =>
+            branches.items.headOption.map { branch =>
+              val phones = for {
+                groups <- branch.contact_groups.toSeq.flatten
+                contact <- groups.contacts
+                if contact.`type` == "phone"
+              } yield contact.text
+              val location = branch.point.map(p => Location(p.lon, p.lat))
+              Info(branch.name, phones, location)
+            }.getOrElse(NotFound)
+        }
+      }
+      .map {
+        case NotFound => teleApi.sendMessage(SendMessage(chatId, "Увы, мы ничего не нашли"))
+        case Info(name, phones, location) =>
+          val phonesText = Option(phones)
+            .filter(_.nonEmpty)
+            .map(_.mkString(", "))
+            .map(p => s"Позвонить: $p\n")
+            .getOrElse("")
+          val text = s"$name\n$phonesText"
+
+          for {
+            callbackF <- callbackId.map(teleApi.answerCallbackQuery(_, None)).getOrElse(Future.successful(()))
+            msgF <- teleApi.sendMessage(SendMessage(chatId, text))
+            locatF <- location.map {
+              case Location(lon, lat) =>
+                teleApi.sendLocation(SendLocation(chatId, lat, lon))
+            }.getOrElse(Future.successful(Done))
+          } yield Done
+      }
+      .map(_ => Done)
+  }
+
+}
+
+object Search {
+  def apply(chatId: Long, text: String)(implicit ec: ExecutionContext, teleApi: TelegramBotAPI, webApi: WebAPI): Future[Done] = {
+    case class Reply(chatId: Long, response: Response)
+    trait Response
+    object UnknownUpdate extends Response
+    object NotFound extends Response
+    case class Firm(id: String, name: String)
+    case class Firms(firms: List[Firm]) extends Response
+
+    def renderReply(reply: Reply) = reply.response match {
+      case UnknownUpdate => SendMessage(reply.chatId, "Какое-то непонятное сообщение")
+      case NotFound => SendMessage(reply.chatId, "Увы, мы ничего не нашли")
+      case Firms(firms) =>
+        val markup = InlineKeyboardMarkup(
+          List(firms.zipWithIndex.map { case (f, i) => InlineKeyboardButton(s"${i + 1}", None, Some(f.id)) })
+        )
+        val text = firms.zipWithIndex.map {
+          case (f, i) => s"${i + 1}. ${f.name}"
+        }.mkString("\n")
+        SendMessage(reply.chatId, text, Some(markup))
+    }
+
+    webApi
+      .searchBranches(text, 1, page = Some(1), pageSize = Some(5))
+      .map { search =>
+        if (search.items.isEmpty) {
+          NotFound
+        }
+        else {
+          Firms(
+            search.items.toList.map { branch =>
+              val address = branch.address_name.map(address => s" ($address)").getOrElse("")
+              val id = branch.id.split('_').head
+              Firm(id, s"${branch.name}$address")
+            }
+          )
+        }
+      }
+      .map(reply => renderReply(Reply(chatId, reply)))
+      .flatMap(teleApi.sendMessage)
+      .map(_ => Done)
+  }
+
+}
